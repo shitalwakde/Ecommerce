c3926156776affbcabd51c8819bce7a71d7e583b
swapnil meshram <swapnil.meshram@kratin.co.in>
2023-10-23 08:22:39 +0530

    connect mongoose database

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,21 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
+
 dotenv.config({ path: "backend/config/config.env" });
 
-//import all routes
+//connecting to database
+connectDatabase()
+
+// Import all routes
 import productRoutes from "./routes/products.js";
 
 app.use("/api/v1", productRoutes);
 
 
 app.listen(process.env.PORT, () => {
-    console.log(
-      `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
-    );
-  });
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
