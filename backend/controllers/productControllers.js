3961c7cca294d778277db5051cec476b3a6932f4
swapnil meshram <swapnil.meshram@kratin.co.in>
2023-10-24 01:15:21 +0530

    handle product resource (CRUD)

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,7 +1,76 @@
+import Product from "../models/product.js";
 
-
+// Create new Product   =>  /api/v1/products
 export const getProducts = async (req, res) => {
+  const product = await Product.find();
+
     res.status(200).json({
-      message: "All Products",
+      product,
     });
   };
+
+
+// Create new Product   =>  /api/v1/admin/products
+export const newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    product,
+  });
+};
+
+
+// Get single product details   =>  /api/v1/products/:id
+export const getProductDetails = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    product,
+  });
+};
+
+
+// Update product details   =>  /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { 
+    new: true 
+  });
+
+
+  res.status(200).json({
+    product,
+  });
+};
+
+
+
+// Delete product   =>  /api/v1/products/:id
+export const deleteProduct = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    message: "Product Deleted",
+  });
+};
