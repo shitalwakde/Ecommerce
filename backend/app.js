be7c5d233c2121f5c87fbe9d0b4041912ef6dbbe
swapnil meshram <swapnil.meshram@kratin.co.in>
2023-10-24 19:44:49 +0530

    error handling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
+import errorMiddleware from "./middlewares/errors.js"
+
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down due to uncaught expection");
+  process.exit(1);
+});
 
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -10,14 +18,28 @@ connectDatabase()
 
 app.use(express.json());
 
+
+
 // Import all routes
 import productRoutes from "./routes/products.js";
 
 app.use("/api/v1", productRoutes);
 
+// Using error middleware
+app.use(errorMiddleware);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+
+//Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
