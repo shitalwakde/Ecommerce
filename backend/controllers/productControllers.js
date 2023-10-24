08b93e7552c1ca6a2b5ebc05c3a4c44cbc65f4de
swapnil meshram <swapnil.meshram@kratin.co.in>
2023-10-25 00:38:40 +0530

    backend filters

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,18 +1,25 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/product.js";
+import APIFilters from "../utils/apiFilters.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
-
-
-// Get All Product   =>  /api/v1/products
+// Create new Product   =>  /api/v1/products
 export const getProducts = catchAsyncErrors(async (req, res) => {
-  const product = await Product.find();
+  const resPerPage = 4;
+  const apiFilters = new APIFilters(Product, req.query).search().filters();
 
-    res.status(200).json({
-      product,
-    });
+  let products = await apiFilters.query;
+  let filteredProductsCount = products.length;
+
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
+
+  res.status(200).json({
+    resPerPage,
+    filteredProductsCount,
+    products,
   });
-
+});
 
 // Create new Product   =>  /api/v1/admin/products
 export const newProduct = catchAsyncErrors(async (req, res) => {
@@ -22,7 +29,6 @@ export const newProduct = catchAsyncErrors(async (req, res) => {
     product,
   });
 });
-
 
 // Get single product details   =>  /api/v1/products/:id
 export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
@@ -37,29 +43,25 @@ export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-
 // Update product details   =>  /api/v1/products/:id
-export const updateProduct = catchAsyncErrors(async (req, res, next) => {
+export const updateProduct = catchAsyncErrors(async (req, res) => {
   let product = await Product.findById(req?.params?.id);
 
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
   }
 
-  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { 
-    new: true 
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+    new: true,
   });
-
 
   res.status(200).json({
     product,
   });
 });
 
-
-
 // Delete product   =>  /api/v1/products/:id
-export const deleteProduct = catchAsyncErrors(async (req, res, next) => {
+export const deleteProduct = catchAsyncErrors(async (req, res) => {
   const product = await Product.findById(req?.params?.id);
 
   if (!product) {
