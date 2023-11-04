377dad3c606c1191aec1e97906c1f650e89a3a39
shital <shitalwakde1991@gmail.com>
2023-11-05 00:43:06 +0530

    Authentication Frontend

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,6 +8,8 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import { Toaster } from "react-hot-toast";
 import ProductDetails from "./components/product/ProductDetails";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 
 function App() {
@@ -21,7 +23,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetails />} />
-
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
 
--- frontend/my-app/src/components/layout/Header.jsx
+++ frontend/my-app/src/components/layout/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import Serach from "./Search";
+import { useGetMeQuery } from "../../redux/api/userApi";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useLazyLogoutQuery } from "../../redux/api/authApi";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const { isLoading } = useGetMeQuery();
+  const [logout] = useLazyLogoutQuery();
+
+  const logoutHandler = () => {
+    logout();
+    navigate(0);
+  }
+
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav className="navbar row">
       <div className="col-12 col-md-3 ps-5">
@@ -12,7 +27,7 @@ const Header = () => {
         </div>
       </div>
       <div className="col-12 col-md-6 mt-2 mt-md-0">
-      <Serach/>
+        <Serach />
       </div>
       <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
         <a href="/cart" style={{ textDecoration: "none" }}>
@@ -25,53 +40,57 @@ const Header = () => {
           </span>
         </a>
 
-        <div className="ms-4 dropdown">
-          <button
-            className="btn dropdown-toggle text-white"
-            type="button"
-            id="dropDownMenuButton"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <figure className="avatar avatar-nav">
-              <img
-                src="../images/default_avatar.jpg"
-                alt="User Avatar"
-                className="rounded-circle"
-              />
-            </figure>
-            <span>User</span>
-          </button>
-          <div
-            className="dropdown-menu w-100"
-            aria-labelledby="dropDownMenuButton"
-          >
-            <a className="dropdown-item" href="/admin/dashboard">
-              {" "}
-              Dashboard{" "}
-            </a>
+        {user ? (
+          <div className="ms-4 dropdown">
+            <button
+              className="btn dropdown-toggle text-white"
+              type="button"
+              id="dropDownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <figure className="avatar avatar-nav">
+                <img
+                  src={user?.avatar ? user?.avatar?.url : "/images/default_avatar.jpg"}
+                  alt="User Avatar"
+                  className="rounded-circle"
+                />
+              </figure>
+              <span>{user?.name}</span>
+            </button>
+            <div
+              className="dropdown-menu w-100"
+              aria-labelledby="dropDownMenuButton"
+            >
+              <Link className="dropdown-item" to="/admin/dashboard">
+                {" "}
+                Dashboard{" "}
+              </Link>
 
-            <a className="dropdown-item" href="/me/orders">
-              {" "}
-              Orders{" "}
-            </a>
+              <Link className="dropdown-item" to="/me/orders">
+                {" "}
+                Orders{" "}
+              </Link>
 
-            <a className="dropdown-item" href="/me/profile">
-              {" "}
-              Profile{" "}
-            </a>
+              <Link className="dropdown-item" to="/me/profile">
+                {" "}
+                Profile{" "}
+              </Link>
 
-            <a className="dropdown-item text-danger" href="/">
-              {" "}
-              Logout{" "}
-            </a>
+              <Link className="dropdown-item text-danger" to="/"  onClick={logoutHandler}>
+              
+                Logout{" "}
+              </Link>
+            </div>
           </div>
-        </div>
-
-        <a href="/login" className="btn ms-4" id="login_btn">
-          {" "}
-          Login{" "}
-        </a>
+        ) : (
+          !isLoading && (
+            <Link to="/login" className="btn ms-4" id="login_btn">
+              {" "}
+              Login{" "}
+            </Link>
+          )
+        )}
       </div>
     </nav>
   );
